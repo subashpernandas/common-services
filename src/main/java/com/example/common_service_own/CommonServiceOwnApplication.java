e9efb0c6628a9bd38b7ee0462667a7bd0d5896db
Subash B <subash.b@msf.ad>
2025-06-15 17:20:50 +0530

    add common service inside

---
+++ src/main/java/com/example/common_service_own/CommonServiceOwnApplication.java
@@ -0,0 +1,13 @@
+package com.example.common_service_own;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonServiceOwnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonServiceOwnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/common_service_own/CommonServiceOwnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.common_service_own;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonServiceOwnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
