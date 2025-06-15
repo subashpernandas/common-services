27334d0cbab8fc10ebb8d1ca3b5ffaa87d48a7d2
Subash B <subash.b@msf.ad>
2025-06-15 19:58:25 +0530

    changes in student repos

--- src/main/java/com/common/icici/repository/StudentRepository.java
+++ src/main/java/com/common/icici/repository/StudentRepository.java
@@ -1,9 +1,13 @@
 package com.common.icici.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.common.icici.entity.Student;
 
 public interface StudentRepository extends JpaRepository<Student, Integer> {
 
+	List<Student> findByDept(@Param("dept") String dept);
 }
